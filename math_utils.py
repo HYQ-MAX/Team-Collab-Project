16aa696cc61247d5c42fbb9f9b2629e290520af2
张帅 <1063348628@qq.com>
2026-03-02 16:00:23 +0800

    feat: 新增两数求和函数sum_two

---
+++ math_utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# coding:utf-8
+# author:
+# datetime:2026/3/2 15:58
+# software: PyCharm
+# Explanation:
+# math_utils.py
+def sum_two(a, b):
+    """
+    两数求和函数
+    :param a: 数字1
+    :param b: 数字2
+    :return: 两数之和
+    """
+    return a + b
